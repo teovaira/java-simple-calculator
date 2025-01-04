5cd9be6e2a156aca09d414bfab5e4efab0cb33b5
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-01-04 12:23:03 +0200

    hotfix completed.try catch added for arithmetic exception

--- src/gr/aueb/cf/calc/Main.java
+++ src/gr/aueb/cf/calc/Main.java
@@ -14,7 +14,12 @@ public class Main {
         return a - b;
     }
 
-    public static int div(int a, int b) {
-        return a / b;
+    public static int div(int a, int b)  {
+        try {
+            return a / b;
+        } catch (ArithmeticException e) {
+            System.err.println("Error. Denominator must not be zero. ");
+            throw e;
+        }
     }
 }
